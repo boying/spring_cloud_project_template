878cdb09f6aec5b08d8cb114bc124d320e32ae89
boying <boyinnju@gmail.com>
2018-05-12 15:41:54 +0800

    fix

--- projects/web/src/main/java/com/jzw/user/Application.java
+++ projects/web/src/main/java/com/jzw/user/Application.java
@@ -4,11 +4,13 @@ package com.jzw.user;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.feign.EnableFeignClients;
 
 @EnableFeignClients
 @EnableDiscoveryClient
+@EnableCircuitBreaker
 @SpringBootApplication(exclude = {DataSourceAutoConfiguration.class})
 public class Application {
 
